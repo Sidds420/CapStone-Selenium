8781658635c0d250d062dfa87113e098093f43b8
Sidds420 <82855104+Sidds420@users.noreply.github.com>
2026-03-10 12:20:39 +0530

    Login

--- Banking.py
+++ Banking.py
@@ -12,3 +12,8 @@ driver.get("https://parabank.parasoft.com/")
 driver.maximize_window()
 time.sleep(2)
 
+# Login
+driver.find_element['xpath','//input[@name="username"]'].send_keys("SK")
+driver.find_element['xpath','//input[@name="password"]'].send_keys("")
+driver.find_element['xpath','//input[@value="Log In"]'].click()
+time.sleep(2)
