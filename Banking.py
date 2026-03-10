aa0e62446ced31a3c2853bd6f01d7264b51914a5
AdityaMohanty3010 <adityamohanty3010@gmail.com>
2026-03-10 12:20:26 +0530

    Register Test

--- Banking.py
+++ Banking.py
@@ -5,10 +5,16 @@ import time
 opts = webdriver.ChromeOptions()
 opts.add_experimental_option("detach",True)
 
-driver=webdriver.Chrome()
+driver=webdriver.Chrome(opts)
 wait = WebDriverWait(driver,10)
 
 driver.get("https://parabank.parasoft.com/")
 driver.maximize_window()
 time.sleep(2)
+
+#Register
+driver.find_element('xpath','//a[text()="Register"]').click()
+driver.find_element('xpath','//input[@name="customer.firstName"]').send_keys("SK")
+driver.find_element('xpath','//input[@name="customer.lastName"]').send_keys("Mohla")
+
 
