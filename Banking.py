c4bab2b3f97c76825a84931c288ace6505eff79c
AdityaMohanty3010 <adityamohanty3010@gmail.com>
2026-03-10 12:28:42 +0530

    Register-update

--- Banking.py
+++ Banking.py
@@ -16,5 +16,13 @@ time.sleep(2)
 driver.find_element('xpath','//a[text()="Register"]').click()
 driver.find_element('xpath','//input[@name="customer.firstName"]').send_keys("SK")
 driver.find_element('xpath','//input[@name="customer.lastName"]').send_keys("Mohla")
+driver.find_element('xpath','//input[@name="customer.address.street"]').send_keys("Whitefield")
+driver.find_element('xpath','//input[@name="customer.address.city"]').send_keys("Bengaluru")
+driver.find_element('xpath','//input[@name="customer.address.state"]').send_keys("Karnataka")
+driver.find_element('xpath','//input[@name="customer.address.zipCode"]').send_keys("500106")
+driver.find_element('xpath','//input[@name="customer.phoneNumber"]').send_keys("9456789054")
+
+
+
 
 
